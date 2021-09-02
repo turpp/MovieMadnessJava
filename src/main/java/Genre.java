add340afd2cfc82050c79fcaa872c7bff50aa4c4
turpp <white.tripp@icloud.com>
2021-09-02 11:48:25 -0500

    added array list for each genre

--- src/main/java/Genre.java
+++ src/main/java/Genre.java
@@ -1,2 +1,26 @@
+import info.movito.themoviedbapi.model.MovieDb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Genre {
+//    public String name;
+//    public int tmdbId;
+    List<MovieDb> actionMovies = new ArrayList<>();
+    List<MovieDb> adventureMovies = new ArrayList<>();
+    List<MovieDb> dramaMovies = new ArrayList<>();
+    List<MovieDb> sciFiMovies = new ArrayList<>();
+    List<MovieDb> thrillerMovies = new ArrayList<>();
+    List<MovieDb> comedyMovies = new ArrayList<>();
+    List<MovieDb> horrorMovies = new ArrayList<>();
+    List<MovieDb> familyMovies = new ArrayList<>();
+    List<MovieDb> mysteryMovies = new ArrayList<>();
+    List<MovieDb> documentaryMovies = new ArrayList<>();
+
+//    public Genre(String name, int id){
+//        this.name = name;
+//        tmdbId = id;
+//    }
+
+
 }
